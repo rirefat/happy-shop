f12c7822c543338acb9bd0fa6b640895a8c498a2
rirefat <rirefat.official@gmail.com>
2022-03-25 03:25:55 +0600

    Installing Bootstrap Package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Focusing</h1>
+        </div>
+    );
+};
+
+export default Header;
