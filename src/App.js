f0c8e4f2cb098c08daed890e3716aac23dbee2ab
rirefat <rirefat.official@gmail.com>
2022-03-25 18:31:59 +0600

    Solve some bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Products from './components/Products/Products';
 
 function App() {
   return (
-    <div className='app'>
-      <Header></Header>
-      <Products></Products>
+    <div className="App">
     </div>
   );
 }
